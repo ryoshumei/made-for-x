df82377808283419551c424317c8a474e1905c87
day <s419922572@gmail.com>
2025-04-28 08:36:55 +0900

    Temporarily disable mercoin route using redirects to 404 page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/mercoin',
+        destination: '/404',
+        permanent: false,
+      },
+      {
+        source: '/mercoin/:path*',
+        destination: '/404',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
